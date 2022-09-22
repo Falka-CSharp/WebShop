c011ae4bbbca1c31ec05837d0107f3f5baa13b05
Vladyslav <vladislavshvetsua@gmail.com>
2022-09-22 22:29:43 +0200

    Added pagination

--- WebShop/Controllers/ProductsController.cs
+++ WebShop/Controllers/ProductsController.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using System.IO;
 using WebShop.Data;
 using WebShop.Models;
+using WebShop.ViewModels;
 
 namespace WebShop.Controllers
 {
@@ -24,10 +26,42 @@ namespace WebShop.Controllers
         }
 
         // GET: Products
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? cid, int? pid, int page=1)
         {
-            var applicationDbContext = _context.Products.Include(p => p.Category).Include(p => p.Producer);
-            return View(await applicationDbContext.ToListAsync());
+            //*
+            const int pageSize = 4;
+
+            //*
+            IQueryable<Product>? products = _context?.Products?.Include(p => p.Category).Include(p => p.Producer);
+            if (cid != null && cid!=0)
+                products = products?.Where(p => p.CategoryId == cid);
+            if (pid != null && pid!=0)
+                products = products?.Where(p => p.ProducerId == pid);
+            //*
+            List<Category>? categories = _context?.Categories?.ToList();
+            List<Producer>? producers = _context?.Producers?.ToList();
+
+            //*
+            categories?.Insert(0, new Category() { Id = 0, CategoryName = "All categories" });
+            producers?.Insert(0, new Producer() { Id = 0, ProducerName = "All producers" });
+
+            //*
+            //int count = await (products!=null ? products.CountAsync() : new Task<int>(()=> { return 0; }));
+            int count = await products.CountAsync();
+            var items = await products.Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
+            PageViewModel pageViewModel = new PageViewModel(count, page, pageSize);
+
+            //*
+            FilterViewModel viewModel = new FilterViewModel()
+            {
+                Products = items,
+                Categories = new SelectList(categories, "Id", "CategoryName"),
+                Producers = new SelectList(producers, "Id", "ProducerName"),
+                PageViewModel = pageViewModel
+            };
+
+            //var applicationDbContext = _context.Products.Include(p => p.Category).Include(p => p.Producer);
+            return View(viewModel);
         }
 
         // GET: Products/Details/5
@@ -68,6 +102,19 @@ namespace WebShop.Controllers
         {
             if (ModelState.IsValid)
             {
+                //*
+                if (image != null)
+                {
+                    var name = Path.Combine(_env.WebRootPath + "/Images", Path.GetFileName(image.FileName));
+                    await image.CopyToAsync(new FileStream(name,FileMode.Create, FileAccess.Write));
+                    product.Image = "Images/" + image.FileName;
+                }
+                else
+                {
+                    product.Image = "Images/default.png";
+                }
+
+
                 _context.Add(product);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
---
+++ WebShop/ViewModels/FilterViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using WebShop.Models;
+namespace WebShop.ViewModels
+{
+    public class FilterViewModel
+    {
+        public IEnumerable<Product>? Products { get; set; }
+        public SelectList? Categories { get; set; }
+        public SelectList? Producers { get; set; }
+        public PageViewModel? PageViewModel { get; set; }
+    }
+}
